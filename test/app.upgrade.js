c66276aece437f98b7a3828daaf734570ec96dee
ZoneTwelve <zonetwelve159@gmail.com>
2022-09-20 10:48:36 +0800

    [update] construct controllerContainer

--- test/app.upgrade.js
+++ test/app.upgrade.js
@@ -4,18 +4,22 @@
 var express = require('express');
 var Modular = require('..');
 
-var router = new Modular.Router( );
+var Controller = new Modular.Controller( );
 
 
-controller.public( 'login', ( ) => { 
+Controller.public( 'login', ( account, password ) => { 
   console.log(`Your account is ${account}, and your password have ${password.length} digits.`);
 });
 
-controller.private( 'register', ( ) => {
+Controller.private( 'register', ( ) => {
   console.log("Registering...");
 });
 
 var app = express( );
 Modular.upgrade( app );
-
+ 
 app.modular( 'sample', { control: controller });
+
+let logRes = app['sample']['control'].login( 'admin', '123456' );
+let regRes = app['sample']['control'].register( );
+console.log( logRes, regRes );
