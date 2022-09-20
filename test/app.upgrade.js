e041262fbea0e44c72cf2cca0ed36d1dc3697ec7
ZoneTwelve <zonetwelve159@gmail.com>
2022-09-20 17:36:14 +0800

    [update] Controller can execute user's functions

--- test/app.upgrade.js
+++ test/app.upgrade.js
@@ -8,18 +8,29 @@ var Controller = new Modular.Controller( );
 
 
 Controller.public( 'login', ( account, password ) => { 
-  console.log(`Your account is ${account}, and your password have ${password.length} digits.`);
+  return `Your account is ${account}, and your password have ${password.length} digits.`;
 });
 
-Controller.private( 'register', ( ) => {
-  console.log("Registering...");
+Controller.private( 'register', ( a, p ) => {
+  // console.log(a, p);
+  // console.log("Registering...");
+  return { account: a, password: p };
 });
 
 var app = express( );
 Modular.upgrade( app );
  
-app.modular( 'sample', { control: controller });
+app.modular( 'sample', { control: Controller });
 
-let logRes = app['sample']['control'].login( 'admin', '123456' );
-let regRes = app['sample']['control'].register( );
-console.log( logRes, regRes );
+let { sample } = app.modules;
+
+console.log( sample );
+
+
+
+let loginResult = sample.control.login( 'test', '123456' );
+let registerResult = sample.control.register( 'test', '123456' );
+
+// let logRes = app['sample']['control'].login( 'admin', '123456' );
+// let regRes = app['sample']['control'].register( );
+console.log( loginResult, registerResult );
