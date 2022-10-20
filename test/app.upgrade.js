92ef416a5d736351c350529a51415c9c31e77a41
ZoneTwelve <zonetwelve159@gmail.com>
2022-10-20 20:32:11 +0800

    [update] Router can be use it

--- test/app.upgrade.js
+++ test/app.upgrade.js
@@ -4,33 +4,49 @@
 var express = require('express');
 var Modular = require('..');
 
+// www: Create express application
+var app = express( );
+var http = require('http');
+Modular.upgrade( app );
+// End of express application
+
+// control.js: sample controller
 var Controller = new Modular.Controller( );
-
-
 Controller.public( 'login', ( account, password ) => { 
   return `Your account is ${account}, and your password have ${password.length} digits.`;
 });
 
 Controller.private( 'register', ( a, p ) => {
-  // console.log(a, p);
-  // console.log("Registering...");
   return { account: a, password: p };
 });
 
-var app = express( );
-Modular.upgrade( app );
- 
-app.modular( 'sample', { control: Controller });
+// route.js: sample router
+var Router = new Modular.Router( );
 
+Router.get('/', ( root, req, res ) => {
+  console.log("----- Request -----");
+  let { self } = root;
+  console.log( self );
+  res.send( 'Hello, World!' );
+});
+
+// End of sample modules
+
+
+// app.js: 
+// app constructor
+// create an sub-modules
+app.modular( 'sample', { control: Controller, route: Router } );
 let { sample } = app.modules;
-
 console.log( sample );
+// End of app.js
 
-
-
+// test
+// run application sub-modules controller
 let loginResult = sample.control.login( 'test', '123456' );
 let registerResult = sample.control.register( 'test', '123456' );
 
-// let logRes = app['sample']['control'].login( 'admin', '123456' );
-// let regRes = app['sample']['control'].register( );
-console.log( loginResult, registerResult );
+
+console.log( {loginResult, registerResult} );
+
+http.createServer( app ).listen( 3000 );
